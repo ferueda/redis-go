67dfaf7445cb925fae68798f50b44036918bc827
ferueda <fe.rueda@outlook.com>
2023-04-15 01:27:56 -0700

    handle multiple pings

--- app/server.go
+++ app/server.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net"
 	"os"
+	"strings"
 )
 
 func main() {
@@ -18,10 +20,31 @@ func main() {
 		os.Exit(1)
 	}
 	defer conn.Close()
-	buf := make([]byte, 1024)
-	if _, err := conn.Read(buf); err != nil {
-		fmt.Println("error reading from client: ", err.Error())
-		os.Exit(1)
+	for {
+		reqData := make([]byte, 1024)
+		reqSize, err := conn.Read(reqData)
+		if err != nil {
+			if err == io.EOF {
+				break
+			} else {
+				fmt.Println("err reading from client: ", err.Error())
+				os.Exit(1)
+			}
+		}
+		var resMessage string
+		reqStrings := strings.TrimSpace(string(reqData[:reqSize]))
+
+		if reqStrings == "PING" {
+			resMessage = "+PONG\r\n"
+		} else if len(reqStrings) >= 4 && reqStrings[0:4] == "PING" {
+			resMessage = "+" + reqStrings[5:] + "\r\n"
+		} else {
+			resMessage = "+PONG\r\n"
+		}
+		resData := []byte(resMessage)
+		_, err = conn.Write(resData)
+		if err != nil {
+			fmt.Println("Error sending response: ", err.Error())
+		}
 	}
-	conn.Write([]byte("+PONG\r\n"))
 }
